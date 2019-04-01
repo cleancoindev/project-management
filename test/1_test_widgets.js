bb0565ff6fa39fb9c68df266a63fc9ae8f5ea9b2
masaun <masanoriuno@gmail.com>
2019-04-01 09:38:00 +0200

    Add test for Widgets contract of getWidget function and addWidget function

--- test/1_test_widgets.js
+++ test/1_test_widgets.js
@@ -15,9 +15,33 @@ contract('Widgets', (accounts) => {
         response = await contract.methods.getWidgetsLength().call();
 
         // Debug
-        console.log('=== response of getWidgetsLength functio ===', response);  // Result: OK
+        console.log('=== response of getWidgetsLength function ===', response);  // Result: OK
     })
 
 
-    
+    it('Execute getWidget function', async () => {
+        const contract = await new web3.eth.Contract(Widgets.abi, Widgets.address);
+        response = await contract.methods.getWidget(0).call();
+
+        // Debug
+        console.log('=== response of getWidget function ===', response);  // Result: OK
+    })
+
+
+
+    // _addr = accounts[1]
+    // _addr = str(accounts[1])
+    //_addr = "0x3e08b4eca537b3908bd40dc9d2d1c60bc52a552b"
+    it('Execute addWidget function', async (_addr) => {
+        //const accounts = await web3.eth.getAccounts();
+        //_addr = accounts[1]
+
+        const contract = await new web3.eth.Contract(Widgets.abi, Widgets.address);
+        response = await contract.methods.addWidget("0x3e08b4eca537b3908bd40dc9d2d1c60bc52a552b").send({ from: accounts[0] });
+        //response = await contract.methods.addWidget(_addr).send({ from: accounts[0] });
+
+        // Debug
+        console.log('=== response of addWidget function ===', response);  // Result: OK
+    })
+
 })
