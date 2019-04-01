af1d0b97847701470450e8c544771a6493fa1f6b
masaun <masanoriuno@gmail.com>
2019-04-01 09:14:27 +0200

    Add test of getWidgetsLength function

--- test/1_test_widgets.js
+++ test/1_test_widgets.js
@@ -1,10 +1,23 @@
 const Widgets = artifacts.require('Widgets.sol')
 
 contract('Widgets', (accounts) => {
-    it('should be tested', async () => {
+    it('Get accounts', async () => {
         const accounts = await web3.eth.getAccounts();
 
+        // Debug
         console.log('=== accounts ===', accounts);
         console.log('=== accounts[0] ===', accounts[0]);
     })
+
+
+    it('Execute getWidgetsLength function', async () => {
+        const contract = await new web3.eth.Contract(Widgets.abi, Widgets.address);
+        response = await contract.methods.getWidgetsLength().call();
+
+        // Debug
+        console.log('=== response of getWidgetsLength functio ===', response);  // Result: OK
+    })
+
+
+    
 })
