9363c5b186036799f6c042a4057339a17a7c93b5
masaun <masanoriuno@gmail.com>
2019-04-02 11:35:38 +0200

    Remove all of Contact function and extra codes

--- contracts/Propose.sol
+++ contracts/Propose.sol
@@ -5,22 +5,13 @@ import "zos-lib/contracts/Initializable.sol";
 
 contract Propose is Initializable {
 
-    // struct Proposal {
-    //     //uint256 proposerId;
-    //     string proposerName;      // name of person who propose
-    //     address proposerAddress;  // address of person who propose
-    //     //string content;   // content of proposal
-    // }
-    // //mapping (address => Proposal) proposal;  // e.g) proposal[addres].name
-    // Proposal[] public proposals;  // e.g) proposal[index].name
-
     struct Proposer {
         //uint256 proposerId;
         string proposerName;      // name of person who propose
         address proposerAddress;  // address of person who propose
         //mapping (address => Proposal[]) proposal;
     }
-    Proposer[] public proposers;  // e.g) proposal[index].name
+    Proposer[] public proposers;  // e.g) proposers[index].proposerName
 
 
     struct Proposal {
@@ -31,13 +22,7 @@ contract Propose is Initializable {
 
 
 
-    struct Contact {
-        string contactName;
-        address contactAddress;
-    }
-    Contact[] public contacts;
-
-    // event CreateProposal(
+    // event CreateProposer(
     //     uint256 indexed _proposerId, 
     //     address indexed _proposerAddress, 
     //     string indexed _name, 
@@ -58,8 +43,7 @@ contract Propose is Initializable {
         return _owner;
     }
 
-    /* @notice Create new proposal */ 
-    //function createProposal(
+    /* @notice Create new proposer */ 
     function createProposer(
         //uint256 _proposerId,
         string memory _proposerName,
@@ -67,18 +51,15 @@ contract Propose is Initializable {
         //string memory _content
     ) public returns (string memory, address) 
     {
-        //Proposal memory proposal = Proposal({
         Proposer memory proposer = Proposer({
             //proposerId: _proposerId,
             proposerName: _proposerName,
             proposerAddress: _proposerAddress
             //content: _content
         });
-
-        //proposals.push(proposal);
         proposers.push(proposer);
 
-        //emit CreateProposal(_proposerId, _proposerAddress, _name, _content);
+        //emit CreateProposer(_proposerId, _proposerAddress, _name, _content);
 
         return (_proposerName, _proposerAddress);
     }
@@ -88,90 +69,21 @@ contract Propose is Initializable {
     function getProposerId() public view returns (uint256 _proposerId) {
         return proposers.length - 1;
     }
-    // function getProposalId() public view returns (uint256 _proposerId) {
-    //     return proposals.length - 1;
-    // }
 
 
-    /* @notice Save new proposal */
+    /* @notice Save new proposer */
     function saveProposer(uint256 _proposerId) public returns (bool success) {
         Proposer storage proposer = proposers[_proposerId];
-        //proposal.proposerId = _proposerId;
+        //proposer.proposerId = _proposerId;
         proposer.proposerName = proposers[_proposerId].proposerName;
         proposer.proposerAddress = proposers[_proposerId].proposerAddress;
-        //proposal.content = _content;
+        //proposer.content = _content;
         return true;
     }
-    // function saveProposal(uint256 _proposerId) public returns (bool success) {
-    //     Proposal storage proposal = proposals[_proposerId];
-    //     //proposal.proposerId = _proposerId;
-    //     proposal.proposerName = proposals[_proposerId].proposerName;
-    //     proposal.proposerAddress = proposals[_proposerId].proposerAddress;
-    //     //proposal.content = _content;
-    //     return true;
-    // }
 
 
-    
-
-
-
-    /* @notice New proposal */ 
-    // function newProposal(
-    //     // uint256 _proposerId,
-    //     string memory _proposerName,
-    //     address _proposerAddress
-    //     //string memory _content
-    // ) public returns (string memory, address) 
-    // {
-    //     Proposal memory proposal = Proposal({
-    //         // proposerId: _proposerId,
-    //         proposerName: _proposerName,
-    //         proposerAddress: _proposerAddress
-    //         //content: _content
-    //     });
-    //     proposals.push(proposal);
-
-    //     // Get new proposerId
-    //     proposerId = proposals.length - 1;
-
-    //     // Save new proposal
-    //     Proposal storage proposal = proposals[proposerId];
-    //     //proposal.proposerId = _proposerId;
-    //     proposal.proposerName = proposals[proposerId].proposerName;
-    //     proposal.proposerAddress = proposals[proposerId].proposerAddress;
-    //     //proposal.content = _content;
-
-    //     //emit CreateProposal(_proposerId, _proposerAddress, _name, _content);
-
-    //     return (_proposerName, _proposerAddress);
-    // }
-    
-
-
-    /* @notice createContace */ 
-    function createContact(
-        string memory _contactName, 
-        address _contactAddress
-    ) public returns (string memory, address) 
-    {
-        Contact memory contact = Contact({
-            contactName: _contactName,
-            contactAddress: _contactAddress
-        });
-        contacts.push(contact);
-
-        return (_contactName, _contactAddress);
-    }
-
-
-
-    /* @notice Get number of total proposal */
+    /* @notice Get number of total proposer */
     function getNumberOfTotalProposer() public view returns (uint) {
         return proposers.length;
     }
-    // function getNumberOfTotalProposal() public view returns (uint) {
-    //     return proposals.length;
-    // }
-    
 }
--- test/1_test_Propose.js
+++ test/1_test_Propose.js
@@ -13,28 +13,10 @@ contract('Propose', (accounts) => {
     it('Execute getNumberOfTotalProposer function', async () => {
         const contract = await new web3.eth.Contract(Propose.abi, Propose.address);
         response = await contract.methods.getNumberOfTotalProposer().call();
-        //response = await contract.methods.getNumberOfTotalProposal().call();
 
         // Debug
         console.log('=== response of getNumberOfTotalProposer function ===', response);  // Result: OK
     })
-
-
-
-    // Successful
-    contactName = "Taro Suzuki"
-    contactAddress = "0x3e08b4eca537b3908bd40dc9d2d1c60bc52a552b"
-
-    it('Execute createContact function', async (_contactName=contactName, _contactAddress=contactAddress) => {
-        const accounts = await web3.eth.getAccounts();
-
-        const contract = await new web3.eth.Contract(Propose.abi, Propose.address);
-        //response = await contract.methods.createContact("鈴木", "0x46eE041Dc142117FD9a9D1011178Df5066e29d42").send({ from: accounts[0] })
-        response = await contract.methods.createContact(contactName, contactAddress).send({ from: accounts[0] });
-        // Debug
-        console.log('=== response of createContact function ===', response);  // Result: OK
-    })
-
 
 
     // Successful
@@ -44,12 +26,10 @@ contract('Propose', (accounts) => {
     //content = "This is test for createProposal contract"
 
     it('Execute createProposer function', async (_proposerName=proposerName, _proposerAddress=proposerAddress) => {
-    //it('Execute createProposal function', async (_proposerName=proposerName, _proposerAddress=proposerAddress) => {
         const accounts = await web3.eth.getAccounts();
 
         const contract = await new web3.eth.Contract(Propose.abi, Propose.address);
         response = await contract.methods.createProposer(proposerName, proposerAddress).send({ from: accounts[0] });
-        //response = await contract.methods.createProposal(proposerName, proposerAddress).send({ from: accounts[0] });
 
         // Debug
         console.log('=== response of createProposer function ===', response);  // Result: OK
@@ -64,15 +44,12 @@ contract('Propose', (accounts) => {
 
         const contract = await new web3.eth.Contract(Propose.abi, Propose.address);
         response_1 = await contract.methods.createProposer(proposerName, proposerAddress).send({ from: accounts[0] });
-        //response_1 = await contract.methods.createProposal(proposerName, proposerAddress).send({ from: accounts[0] });
 
         //proposerId = 0
         proposerId = await contract.methods.getProposerId().call();
-        //proposerId = await contract.methods.getProposalId().call();
         console.log('=== response of getProposerId function ===', proposerId);  // Result: OK
         
         response_2 = await contract.methods.saveProposer(proposerId).send({ from: accounts[0] });
-        //response_2 = await contract.methods.saveProposal(proposerId).send({ from: accounts[0] });
         console.log('=== response of saveProposer function ===', response_2);  // Result: OK
     })
 })
